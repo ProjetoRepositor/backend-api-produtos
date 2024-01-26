0eae76f35e4dcdd0e7a3930791660a0dfde59555
CaioRuizz <caio.ruiz00@gmail.com>
2024-01-26 18:30:48 -0300

    Adicionando permissão dynamodb

--- App/src/App/obj/Release/net6.0/linux-x64/App.AssemblyInfo.cs
+++ App/src/App/obj/Release/net6.0/linux-x64/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400c3600bd0bec17160380c26a6bcd2703787ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
